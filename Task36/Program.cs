93db54617c0bdddd9c653a41b44c4a53831e60b8
«Olga» <rusalochka7913@gmail.com>
2022-08-13 19:50:34 +0300

    Task38

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -3,30 +3,29 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
-int numberElements = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Введи количество элементов массива: ");
+int numberElements = Convert.ToInt32(Console.ReadLine()); 
 
 int RandomNumbers(int numberElements, int min, int max)
-{
-    int[] randomNumbers = new int[numberElements];
-    int sumElements = 0;
-    Console.Write("Получившийся массив: ");
+  {
+  int[] randomNumbers = new int[numberElements];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
 
-    for (int i = 0; i < randomNumbers.Length; i++)
-    {
-        randomNumbers[i] = new Random().Next(min, max);
+    for (int i = 0; i <randomNumbers.Length; i++ ){
+      randomNumbers[i] = new Random().Next(min, max);
 
-        Console.Write(randomNumbers[i] + " ");
+      Console.Write(randomNumbers[i] + " ");
 
-        if (i % 2 != 1)
-        {
-            sumElements = sumElements + randomNumbers[i];
-        }
+      if (i % 2 != 1)
+      {
+        sumElements = sumElements + randomNumbers[i];
+      }
     }
-    return sumElements;
-}
+  return sumElements;
+  }
 
-int randomNumbers = RandomNumbers(numberElements, 1, 10);
+int randomNumbers =  RandomNumbers(numberElements, 1, 10);
 
 Console.WriteLine($"\nСумма элементов, стоящих на нечётных позициях: {randomNumbers}");
-
 
