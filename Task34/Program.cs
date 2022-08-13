c5317eba5f3e122e51dd1ad2921005caad15ed86
«Olga» <rusalochka7913@gmail.com>
2022-08-13 19:46:47 +0300

    Task36

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -4,34 +4,28 @@
 
 int[] numbers = new int[10];
 
-void FillArray(int[] array, int min, int max)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(min, max);
-    }
+void FillArray(int[] array, int min, int max){
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
 }
 
-void WriteArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
+void WriteArray(int[] array){
+    for (int i = 0; i<array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
 }
 
-int QuantityPositive(int[] array)
-{
+int QuantityPositive(int[] array){
     int quantity = 0;
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i<array.Length; i++ ){
+    if (array[i] % 2 == 1)
     {
-        if (array[i] % 2 == 1)
-        {
-            quantity++;
-        }
+      quantity++;
     }
-    return quantity;
+  }
+  return quantity;
 }
 
 FillArray(numbers, 100, 1000);
